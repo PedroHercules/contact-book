cbd8fc829188dc8c40999344f90d3fb80c4facfd
PedroHercules <pedroherculesdantas@gmail.com>
2023-02-11 19:12:50 -0300

    first commit

---
+++ styles/Globals.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  html {
+    font-size: 65%;
+  }
+`
---
+++ styles/Texts.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 3em;
+`
+
+export const Subtitle = styled.h2`
+  font-size: 2rem;
+`
+
+export const Paragraph = styled.p`
+  font-size: 1.2rem;
+`
